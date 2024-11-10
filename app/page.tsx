e95e87b1fcde2d128cc9a1fa873b37fa8e8c0e68
Edevar <edevar.ads@gmail.com>
2024-11-10 20:29:44 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return ( <h1 className="text-red-600 p-5">hello world</h1>);
-}
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-600">hello world</h1>
+    </div>
+  );
+};
 
 export default Home;
